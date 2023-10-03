a9d8933f0df81f280bc871e9f159e237a9c5cba9
martsiv <mykola.martsiv@gmail.com>
2023-10-04 00:19:33 +0300

    Add project files.

---
+++ ClientScreenshotWPF/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace ClientScreenshotWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+        static int port = 7777;
+        CancellationTokenSource cancelTokenSource;
+        CancellationToken token;
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+        }
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            await GetScreenshot();
+        }
+        private async void GetScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            await GetScreenshot();
+        }
+        public async Task GetScreenshot()
+        {
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, port);
+            using TcpClient client = new TcpClient();
+
+            try
+            {
+                // виконуємо підключення
+                await client.ConnectAsync(endPoint);
+
+                using var writer = new StreamWriter(client.GetStream());
+                {
+                    writer.WriteLineAsync("Get screenshot");
+                    writer.Flush();
+                }
+                //Зчитуємо скрішот з серверу і десеріалізуємо його
+                var info = await JsonSerializer.DeserializeAsync<FileTransferInfo>(client.GetStream());
+                //Показуємо скріншот у вікні View WPF 
+                SetImageIntoViewAsync(info.Data);
+                //Перевіряємо чи існує шлях для збереження скріншоту
+                if (!Directory.Exists("Files"))
+                    Directory.CreateDirectory("Files");
+                //Зберігаємо скріншот на клієнті
+                await File.WriteAllBytesAsync($"Files/{info.Name}", info.Data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+
+        }
+        private void SetImageIntoViewAsync(byte[] data)
+        {
+            BitmapImage bitmap = new BitmapImage();
+
+            // Асинхронно створюємо MemoryStream і завантажуємо дані в нього
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                bitmap.BeginInit();
+                bitmap.StreamSource = stream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+            }
+
+            // Призначте BitmapImage об'єкт властивості Source вашого елемента Image в XAML
+            myImage.Source = bitmap;
+        }
+
+        private void CheckBoxIntervalAction_Checked(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(IntervalTextBox.Text, out int intervalSeconds))
+            {
+                timer.Interval = TimeSpan.FromSeconds(intervalSeconds);
+                timer.Start();
+            }
+        }
+
+        private void CheckBoxIntervalAction_Unchecked(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+
+        }
+    }
+}
---
+++ ServerScreenshotsConsole/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text.Json;
+using Shared;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace ServerScreenshotsConsole
+{
+    class Program
+    {
+        static IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+        static int port = 7777;
+        static async Task Main(string[] args)
+        {
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, port);
+
+            // створюємо екземпляр сервера вказуючи кінцеву точку для приєднання
+            TcpListener server = new TcpListener(localEndPoint);
+            // запускаємо прослуховування вказаної кінцевої точки
+            server.Start(10);
+
+            await StartListeningAsync(server);
+        }
+        static async Task StartListeningAsync(TcpListener server)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("\tWaiting for command...");
+                    // отримуємо зв'язок з клієнтом асинхронно
+                    TcpClient client = await server.AcceptTcpClientAsync(); // waiting...
+
+                    Task.Run(async () => await HandleClientAsync(client));
+                    //await HandleClientAsync(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+        static async Task HandleClientAsync(TcpClient client)
+        {
+            //Імітація затримки в роботі сервера, щоб потестувати чи не фрізить інтерфейс
+            //await Task.Delay(5000);
+
+            using var reader = new StreamReader(client.GetStream());
+            var response = await reader.ReadLineAsync();
+            Console.WriteLine(response);
+
+            FileTransferInfo info = new FileTransferInfo();
+            info.Name = Path.GetFileName($"Screen_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.jpg");
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // Зберігаємо зображення у форматі PNG в потік
+                ScreenCapture.GetFullScreenshot().Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // Отримуємо масив байтів із потоку
+                info.Data = stream.ToArray();
+            }
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                // серіалізуємо об'єкт класу та відправляємо його на клієнта асинхронно
+                await JsonSerializer.SerializeAsync(stream, info);
+            }
+        }
+    }
+
+    public class ScreenCapture
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetProcessDPIAware();
+        static Size GetMonitorSize()
+        {
+            IntPtr hwnd = Process.GetCurrentProcess().MainWindowHandle;
+            using Graphics g = Graphics.FromHwnd(hwnd);
+            return new Size((int)g.VisibleClipBounds.Width, (int)g.VisibleClipBounds.Height);
+        }
+        static Bitmap TakeScreenshot(Size size)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height);
+            using Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(Point.Empty, Point.Empty, bmp.Size);
+            return bmp;
+        }
+        public static Bitmap GetFullScreenshot()
+        {
+            SetProcessDPIAware();
+            Size monitorSize = GetMonitorSize();
+            return TakeScreenshot(monitorSize);
+        }
+        public static void SaveScreenshot(string path, ImageFormat format)
+        {
+            GetFullScreenshot().Save(path, format);
+        }
+    }
+}
